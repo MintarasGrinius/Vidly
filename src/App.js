2e8e8d415c824353b22e1b654bd76b0e006ad858
Mintaras Grinius <mintarasg@gmail.com>
2020-12-15 10:19:08 +0200

    Movies listed form JSON and delte button added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { getMovies } from './services/fakeMovieService';
+import Movies from './movies';
 
 function App() {
+  
+
   return (
     <main className="container">
-      <h1>Hello World</h1>
+
+
+      <Movies />
+    
     </main>
   );
 }
